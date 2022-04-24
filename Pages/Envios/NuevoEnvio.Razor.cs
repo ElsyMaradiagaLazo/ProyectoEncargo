1b1b8dfeb6a1c3c14ef65c5a2f76e06c549c2bee
ElsyMaradiagaLazo <98985096+ElsyMaradiagaLazo@users.noreply.github.com>
2022-04-24 17:26:27 -0600

    Add files via upload

---
+++ Pages/Envios/NuevoEnvio.Razor.cs
@@ -0,0 +1,42 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Modelo;
+using ProyectoEncargos.Interfaces;
+
+namespace ProyectoEncargos.Pages.Envios
+{
+    partial class NuevoEnvio
+    {
+        [Inject] private IEnvioServicio usuarioServicio { get; set; }
+        [Inject] private NavigationManager navigationManager { get; set; }
+        [Inject] SweetAlertService Swal { get; set; }
+
+        private Envio ship = new Envio();
+
+        protected async Task Guardar()
+        {
+            if (string.IsNullOrEmpty(ship.CodigoEnvio) || string.IsNullOrEmpty(ship.Remitente) || string.IsNullOrEmpty(ship.Descripcion))
+            {
+                return;
+            }
+
+            bool inserto = await usuarioServicio.Nuevo(ship);
+            if (inserto)
+            {
+                await Swal.FireAsync("Felicidades!", "Se ha realizado su orden de envio.", SweetAlertIcon.Success);
+            }
+            else
+            {
+                await Swal.FireAsync("Error!!", "No se pudo realizar su orden.", SweetAlertIcon.Error);
+            }
+            navigationManager.NavigateTo("/Envios");
+
+        }
+
+        protected async Task Cancelar()
+        {
+            navigationManager.NavigateTo("/Envios");
+        }
+    }
+}
+
